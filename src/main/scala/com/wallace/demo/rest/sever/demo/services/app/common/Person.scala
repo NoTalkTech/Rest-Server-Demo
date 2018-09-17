b2e57667a9af509a3763d2acb166b22cb215108b
huangbiyu <h417652303@163.com>
2018-09-17 23:16:01 +0800

    20180827 wallace huang: update Person.scala

--- src/main/scala/com/wallace/demo/rest/sever/demo/services/app/common/Person.scala
+++ src/main/scala/com/wallace/demo/rest/sever/demo/services/app/common/Person.scala
@@ -1,6 +1,33 @@
 package com.wallace.demo.rest.sever.demo.services.app.common
 
+import spray.json._
+
 /**
   * Created by 10192057 on 2016/6/17.
   */
 case class Person(name: List[String], age: List[Int])
+
+case class NewPerson(name: String, age: Int)
+
+
+object NewPersonProtocol extends DefaultJsonProtocol {
+
+  implicit object NewPersonFormat extends RootJsonFormat[NewPerson] {
+    override def write(person: NewPerson): JsValue = JsObject("name" -> JsString(person.name), "age" -> JsNumber(person.age))
+
+    override def read(json: JsValue): NewPerson = json match {
+      case JsArray(Vector(JsString(name), JsNumber(age))) => NewPerson(name, age.toInt)
+      case _ => deserializationError("Person expected")
+    }
+  }
+
+}
+
+object Demo {
+  def main(args: Array[String]): Unit = {
+    import NewPersonProtocol._
+    val newP = NewPerson("wallace", 23)
+    val json = newP.toJson
+    println(json.prettyPrint)
+  }
+}
